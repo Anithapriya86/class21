0df517208503a590d52931c72b589fd3fb8f76d3
Anitha Priya Peter <anithapriya86@gmail.com>
2020-10-12 05:23:10 +0530

    Add files via upload

---
+++ IvyLibrary.js
@@ -0,0 +1,15 @@
+//boolean values - true (working), false(not working)
+
+//movingRect as object1 and fixedREct as object2
+function isTouching(object1,object2){
+    if (object1.x - object2.x < object2.width/2 + object1.width/2
+      && object2.x - object1.x < object2.width/2 + object1.width/2
+      && object1.y - object2.y < object2.height/2 + object1.height/2
+      && object2.y - object2.y < object2.height/2 + object1.height/2) {
+      
+      return true;
+    }
+    else {
+      return false;
+    } 
+  }
